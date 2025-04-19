dc5368b9c37e014099287545089dadb33c635364
Mohammad_Kamal <mohammad.kamal.swe@gmail.com>
2025-04-19 22:45:47 +0200

    Update repositories

---
+++ src/main/java/com/mohammadkamal/stockwatcher/repository/StockAlertRepository.java
@@ -0,0 +1,13 @@
+package com.mohammadkamal.stockwatcher.repository;
+
+import com.mohammadkamal.stockwatcher.model.StockAlert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface StockAlertRepository extends JpaRepository<StockAlert, Long> {
+
+    List<StockAlert> findByUser_Id(Long userID);
+}
